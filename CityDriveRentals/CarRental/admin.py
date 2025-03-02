f8a35c41f56ff201aa863e63a55e4d9c6428377e
Steaven <mirandsteavenjohann@gmail.com>
2025-03-03 07:03:33 +0800

    deleted the files for this branch

--- CityDriveRentals/CarRental/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Customer, Vehicle, Booking, Review
-
-# Register your models here.
-
-admin.site.register(Customer)
-admin.site.register(Vehicle)
-admin.site.register(Booking)
-admin.site.register(Review)
